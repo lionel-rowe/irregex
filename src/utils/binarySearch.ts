4be3e8cbd6cb70b48cd609b3ff0a9b240f1daf5d
lionel-rowe <lionel.rowe@gmail.com>
2025-03-25 18:49:54 +0800

    Fix whitespace

--- src/utils/binarySearch.ts
+++ src/utils/binarySearch.ts
@@ -1,5 +1,5 @@
 /**
- * A non-exact binary search (unlike Deno `@std`'s current	 implementation, which just returns `null` if not found).
+ * A non-exact binary search (unlike Deno `@std`'s current implementation, which just returns `null` if not found).
  *
  * Uses same logic as C#'s [`Array.BinarySearch`](https://learn.microsoft.com/en-us/dotnet/api/system.array.binarysearch#system-array-binarysearch(system-array-system-object)), which returns:
  * > The index of the specified value in the specified array, if value is found; otherwise, a negative number. If value is not found and value is less than one or more elements in array, the negative number returned is the bitwise complement of the index of the first element that is larger than value. If value is not found and value is greater than all elements in array, the negative number returned is the bitwise complement of (the index of the last element plus 1).
