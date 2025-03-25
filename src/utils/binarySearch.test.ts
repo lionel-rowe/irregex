2e26e1e358b2ec1a1ce7793fd70ba9d3abd1f63f
lionel-rowe <lionel.rowe@gmail.com>
2025-03-25 19:52:08 +0800

    Use iteration instead of recursion for `binarySearch`

--- src/utils/binarySearch.test.ts
+++ src/utils/binarySearch.test.ts
@@ -1,27 +1,67 @@
 import { assertEquals } from '@std/assert'
 import { binarySearch } from './binarySearch.ts'
+import { assertSpyCalls, spy } from '@std/testing/mock'
 
-Deno.test(binarySearch.name, () => {
-	const arr = [0, 10, 200, 3_000, 40_000, 500_000]
+Deno.test(binarySearch.name, async (t) => {
+	await t.step('empty array', () => {
+		assertEquals(binarySearch([], -1), -1)
+		assertEquals(binarySearch([], 0), -1)
+		assertEquals(binarySearch([], 1), -1)
+	})
 
-	const fn = () => {
-		assertEquals(binarySearch(arr, -1), -1)
-		assertEquals(binarySearch(arr, 0), 0)
-		assertEquals(binarySearch(arr, 10), 1)
-		assertEquals(binarySearch(arr, 200), 2)
-		assertEquals(binarySearch(arr, 3_000), 3)
-		assertEquals(binarySearch(arr, 40_000), 4)
-		assertEquals(binarySearch(arr, 40_001), -6)
-		assertEquals(binarySearch(arr, 500_000), 5)
-		assertEquals(binarySearch(arr, 500_001), -7)
-	}
+	await t.step('single element', () => {
+		assertEquals(binarySearch([0], -1), -1)
+		assertEquals(binarySearch([0], 0), 0)
+		assertEquals(binarySearch([0], 1), -2)
+	})
 
-	fn()
+	await t.step('multiple elements', () => {
+		const arr = [0, 10, 200, 3_000, 40_000, 500_000]
 
-	assertEquals(binarySearch(arr, 6_000_000), -7)
-	arr.push(6_000_000)
-	assertEquals(binarySearch(arr, 6_000_000), 6)
-	assertEquals(binarySearch(arr, 70_000_000), -8)
+		const fn = () => {
+			assertEquals(binarySearch(arr, -1), -1)
+			assertEquals(binarySearch(arr, 0), 0)
+			assertEquals(binarySearch(arr, 10), 1)
+			assertEquals(binarySearch(arr, 200), 2)
+			assertEquals(binarySearch(arr, 3_000), 3)
+			assertEquals(binarySearch(arr, 40_000), 4)
+			assertEquals(binarySearch(arr, 40_001), -6)
+			assertEquals(binarySearch(arr, 500_000), 5)
+			assertEquals(binarySearch(arr, 500_001), -7)
+		}
 
-	fn()
+		fn()
+
+		assertEquals(binarySearch(arr, 6_000_000), -7)
+		arr.push(6_000_000)
+		assertEquals(binarySearch(arr, 6_000_000), 6)
+		assertEquals(binarySearch(arr, 70_000_000), -8)
+
+		fn()
+	})
+
+	await t.step('algorithm correctness - number of loop iterations', () => {
+		/** `Math.trunc` calls act as a proxy for the number of loop iterations, as it's called once per iteration */
+		const spyLoopIterations = () => spy(Math, 'trunc')
+
+		const arr = Array.from({ length: 1_000_000 }, (_, i) => i)
+
+		{
+			using iterations = spyLoopIterations()
+			const searchVal = 499_999
+			const result = binarySearch(arr, searchVal)
+
+			assertEquals(result, 499_999)
+			assertSpyCalls(iterations, 1)
+		}
+
+		{
+			using iterations = spyLoopIterations()
+			const searchVal = 499_999.1
+			const result = binarySearch(arr, searchVal)
+
+			assertEquals(result, -500_001)
+			assertSpyCalls(iterations, 19)
+		}
+	})
 })
--- src/utils/binarySearch.ts
+++ src/utils/binarySearch.ts
@@ -1,19 +1,26 @@
 /**
- * A non-exact binary search (unlike Deno `@std`'s current implementation, which just returns `null` if not found).
+ * A binary search that accounts for non-exact matches.
  *
- * Uses same logic as C#'s [`Array.BinarySearch`](https://learn.microsoft.com/en-us/dotnet/api/system.array.binarysearch#system-array-binarysearch(system-array-system-object)), which returns:
- * > The index of the specified value in the specified array, if value is found; otherwise, a negative number. If value is not found and value is less than one or more elements in array, the negative number returned is the bitwise complement of the index of the first element that is larger than value. If value is not found and value is greater than all elements in array, the negative number returned is the bitwise complement of (the index of the last element plus 1).
- *
- * @param haystack The array to search. This **MUST** already be sorted in ascending order.
+ * @param haystack The array to search, which **MUST** be sorted in ascending order.
  * @param needle The value to search for.
+ * @returns
+ * - If `needle` is matched exactly, the index of `needle`
+ * - If any element is larger than `needle`, the bitwise complement of its index
+ * - Otherwise, the bitwise complement of `haystack.length`
+ *
+ * Logic is based on C#'s [`Array.BinarySearch`](https://learn.microsoft.com/en-us/dotnet/api/system.array.binarysearch#system-array-binarysearch(system-array-system-object)).
  */
-export function binarySearch(haystack: number[], needle: number): number {
-	return _binarySearch(haystack, needle, [0, haystack.length - 1])
-}
+export function binarySearch(haystack: readonly number[], needle: number): number {
+	let start: number, end: number, mid: number
 
-function _binarySearch(haystack: number[], needle: number, [start, end]: [number, number]) {
-	if (start > end) return ~start
-	const mid = Math.floor((start + end) / 2)
-	if (haystack[mid] === needle) return mid
-	return _binarySearch(haystack, needle, haystack[mid] > needle ? [start, mid - 1] : [mid + 1, end])
+	for (
+		start = 0, end = haystack.length - 1;
+		start <= end;
+		haystack[mid]! < needle ? start = mid + 1 : end = mid - 1
+	) {
+		mid = Math.trunc((start + end) / 2)
+		if (haystack[mid] === needle) return mid
+	}
+
+	return ~start
 }
